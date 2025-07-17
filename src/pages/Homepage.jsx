b5c5f58d5e4aa90c06a1693ed0a95cbb9961c056
KhunRen <rendidi.abc@gmail.com>
2025-07-17 23:51:42 +0700

    ???

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,42 +1,42 @@
-import React, { useEffect, useRef } from 'react';
-import { useLocation } from 'react-router-dom';
+  import React, { useEffect, useRef } from 'react';
+  import { useLocation } from 'react-router-dom';
 
-import SectionHeader from '../components/homepage/SectionHeader';
-import SectionAboutSandbox from '../components/homepage/SectionAboutSandbox';
-import SectionSolutionSandbox from '../components/homepage/SectionSolutionSandbox';
-import SectionReadyToUse from '../components/homepage/SectionReadyToUse';
-import SectionParallax from '../components/homepage/SectionParallax';
-import SectionContactUs from '../components/homepage/SectionContactUs';
-import SectionWhyChoose from '../components/homepage/SectionWhyChoose'
-import SectionPortfolio from '../components/homepage/SectionPortfolio'
+  import SectionHeader from '../components/homepage/SectionHeader';
+  import SectionAboutSandbox from '../components/homepage/SectionAboutSandbox';
+  import SectionSolutionSandbox from '../components/homepage/SectionSolutionSandbox';
+  import SectionReadyToUse from '../components/homepage/SectionReadyToUse';
+  import SectionParallax from '../components/homepage/SectionParallax';
+  import SectionContactUs from '../components/homepage/SectionContactUs';
+  import SectionWhyChoose from '../components/homepage/SectionWhyChoose'
+  import SectionPortfolio from '../components/homepage/SectionPortfolio'
 
-const Homepage = () => {
-  const location = useLocation();
-  const contactRef = useRef(null);
+  const Homepage = () => {
+    const location = useLocation();
+    const contactRef = useRef(null);
 
-  // Logika scroll ke section #kontak-kami (sudah disederhanakan)
-  useEffect(() => {
-    const scrollTimeout = setTimeout(() => {
-      if (location.hash === '#kontak-kami' && contactRef.current) {
-        contactRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
-      }
-    }, 200);
+    // Logika scroll ke section #kontak-kami (sudah disederhanakan)
+    useEffect(() => {
+      const scrollTimeout = setTimeout(() => {
+        if (location.hash === '#kontak-kami' && contactRef.current) {
+          contactRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+      }, 200);
 
-    return () => clearTimeout(scrollTimeout);
-  }, [location]);
+      return () => clearTimeout(scrollTimeout);
+    }, [location]);
 
-  return (
-    <div className='bg-black'>
-      <SectionHeader />
-      <SectionAboutSandbox />
-      <SectionSolutionSandbox />
-      <SectionReadyToUse />
-      <SectionParallax topSection={<SectionWhyChoose />} bottomSection={<SectionPortfolio />} /> {/* Komponen ini sekarang mandiri */}
-      <div id="kontak-kami" ref={contactRef}>
-        <SectionContactUs />
+    return (
+      <div className='bg-black'>
+        <SectionHeader />
+        <SectionAboutSandbox />
+        <SectionSolutionSandbox />
+        <SectionReadyToUse />
+        <SectionParallax topSection={<SectionWhyChoose />} bottomSection={<SectionPortfolio />} /> {/* Komponen ini sekarang mandiri */}
+        <div id="kontak-kami" ref={contactRef}>
+          <SectionContactUs />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default Homepage;
+  export default Homepage;
